01985516db3acf6a3f432da3e94e76da34a9811f
Michael Zeisel <mhzeisel@gmail.com>
2022-05-15 19:25:25 -0400

    Giot it to work, I can now talk to myself as myself

--- AI_chatbot/training.py
+++ AI_chatbot/training.py
@@ -1,16 +1,19 @@
-#https://www.youtube.com/watch?v=1lwddP0KUEg&list=WL&index=2&t=1s
 import random
 import json
 import pickle
 import numpy as np
+
 import nltk
 from nltk.stem import WordNetLemmatizer
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
+
 intents = json.loads(open('intents.json').read())
+
 words = []
 classes = []
 documents = []
@@ -30,14 +33,14 @@ words = sorted(set(words))
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
 
 for document in documents:
     bag = []
-    word_patterns = documents[0]
+    word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
@@ -62,6 +65,9 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.model')
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbotmodel.h5', hist)
+
+#model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+#model.save('chatbot_model.model')
 print("Done")
