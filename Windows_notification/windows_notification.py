564eaf5f7e1d4f430a768bb2ccd5541ed1f21958
Michael Zeisel <mhzeisel@gmail.com>
2022-05-17 06:03:19 -0400

    New fun learning, Created a task to run this everyday to study

--- Windows_notification/windows_notification.py
+++ Windows_notification/windows_notification.py
@@ -1,5 +1,17 @@
-from win10toast import ToastNotifier
+import time
+from winotify import Notification, audio
 
-toaster = ToastNotifier()
+toast = Notification(app_id="NeuralNine Script",
+                    title="Important Info",
+                    msg="Learn Python & Machine Learning",
+                    duration="long",
+                    icon=r"C:\Users\mhzei\Documents\GitHub\PythonSmallProjects\Windows_notification\neural.png")
 
-toaster.show_toast("Title", "Message", duration=2)
+toast.add_actions(label="Click Me", launch="https://neuralnine.com/")
+toast.add_actions(label="YouTube", launch="https://www.youtube.com/channel/UC8wZnXYK_CGKlBcZp-GxYPA")
+
+toast.set_audio(audio.LoopingAlarm, loop=True)
+
+
+
+toast.show()
